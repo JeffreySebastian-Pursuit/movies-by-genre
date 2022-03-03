0c4127ae9da27737ff358154b34dd704cc3b044c
Jeffrey <jeffreysebastian@pursuit.org>
2022-03-03 00:09:32 -0500

    backend environment setup

---
+++ BackEnd/db/schema.sql
@@ -0,0 +1,14 @@
+DROP DATABASE IF EXISTS movies_dev;
+CREATE DATABASE movies_dev;
+
+\c movies_dev;
+
+CREATE TABLE movies_genre(
+    id SERIAL PRIMARY KEY,
+    genre TEXT NOT NULL,
+    title TEXT NOT NULL,
+    thumbnail TEXT NOT NULL,
+    year INTEGER NOT NULL,
+    synopsis TEXT NOT NULL,
+    duration NUMERIC NOT NULL
+)
