a67acaa57a4fbe36cc0cc804b0bff2a0a691a3c1
Jeffrey <jeffreysebastian@pursuit.org>
2022-03-04 17:11:15 -0500

    create a post queries

--- BackEnd/controllers/moviesController.js
+++ BackEnd/controllers/moviesController.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const movies = express.Router();
 
-const { fetchAllMovies } = require("../queries/movies");
+const { fetchAllMovies, getMovie, createMovie } = require("../queries/movies");
 
 
 movies.get('/', async (req, res) => {
@@ -9,6 +9,19 @@ movies.get('/', async (req, res) => {
     res.json(movies)
 })
 
+movies.get('/:id', async (req, res) =>{
+    const movie=  await getMovie(req.params.id)
+    if(movie){
+        res.json(movie)
+    }else{
+        res.json({success: false, error: true, message: 'invalid'})
+    }
+})
 
+movies.post('/', async (req, res) => {
+    const newMovie = req.body;
+    const result = await createMovie(newMovie);
+    res.json(result)
+})
 
 module.exports = movies;
--- BackEnd/queries/movies.js
+++ BackEnd/queries/movies.js
@@ -1,14 +1,32 @@
 const db = require("../db/dbConfig.js");
 
-const fetchAllMovies = async() => {
-    try {
-        const movies = await db.any('SELECT * FROM movies_genre');
-        return {success: true, payload: movies}
-    } catch (error) {
-        console.log(error)
-        return {success: false, payload: error}
-    }
-}
+const fetchAllMovies = async () => {
+  try {
+    const movies = await db.any("SELECT * FROM movies_genre");
+    return { success: true, payload: movies };
+  } catch (error) {
+    console.log(error);
+    return { success: false, payload: error };
+  }
+};
 
+const getMovie = async (id) => {
+  return await db.oneOrNone("SELECT * FROM movies_genre WHERE id=$1", id);
+};
 
-module.exports =  {fetchAllMovies}
+const createMovie = async (newMovie) => {
+  const { genre, title, thumbnail, year, synopsis, duration, favorite } =
+    newMovie;
+  try {
+    const movie = await db.one(
+      "INSERT INTO movies_genre (genre, title, thumbnail, year, synopsis, duration, favorite) VALUES($1, $2, $3, $4, $5, $6, $7) RETURNING *",
+      [genre, title, thumbnail, year, synopsis, duration, favorite]
+    );
+    return {success: true, payload: movie}
+  } catch (error) {
+    console.log(error);
+    return {success: false, payload: error}
+  }
+};
+
+module.exports = { fetchAllMovies, getMovie , createMovie};
