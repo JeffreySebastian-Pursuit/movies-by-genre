15a9753f2ca0050ec21b479e68436fd8d18a5ca5
Jeffrey <jeffreysebastian@pursuit.org>
2022-03-17 15:00:07 -0400

    heroku deployment q

---
+++ BackEnd/db/prod_schema.sql
@@ -0,0 +1,31 @@
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS movies CASCADE;
+DROP TABLE IF EXISTS comments CASCADE;
+
+
+CREATE TABLE users(
+    id SERIAL PRIMARY KEY,
+    display_name VARCHAR(40),
+    photo_url TEXT
+);
+CREATE TABLE movies(
+    id SERIAL PRIMARY KEY,
+    genre TEXT NOT NULL,
+    title TEXT NOT NULL,
+    thumbnail TEXT NOT NULL,
+    year INTEGER NOT NULL,
+    synopsis TEXT NOT NULL,
+    duration NUMERIC NOT NULL,
+    favorite INTEGER,
+    user_id INT REFERENCES users (id) on DELETE CASCADE
+);
+
+
+CREATE TABLE comments(
+    id SERIAL PRIMARY KEY,
+    comment TEXT,
+    movies_id INTEGER REFERENCES movies (id) ON DELETE CASCADE,
+    user_id INTEGER REFERENCES users (id) ON DELETE CASCADE,
+    dt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    
+);
